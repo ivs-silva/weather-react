232f3eaded61910e2369ae6b966cb7b909e0062f
ivs-silva <129681084+ivs-silva@users.noreply.github.com>
2024-01-26 10:35:42 +0100

    Update Footer.js
    changed url

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import "./styles.css";
 export default function Footer() {
   return (
     <small className="cardstyle">
-      <a href="https://github.com/ivs-silva/SheCodesPlus" target="_blank" rel="noreferrer">
+      <a href="https://github.com/ivs-silva/weather-react.git" target="_blank" rel="noreferrer">
         Open-Source Code
       </a>
       by Ivonne Silva
